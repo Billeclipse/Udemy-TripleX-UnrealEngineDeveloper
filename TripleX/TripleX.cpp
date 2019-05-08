1a97da152e494072079568ec647032c19244fc6d
Billeclipse <billchelseaaek@hotmail.com>
2019-05-08 15:48:36 +0300

    Section 2 : Trple X - Write Pure C++ - 2019 Version // Lectures : 14-18

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
 int main(){
+    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+    std::cout << "Enter the correct code to continue...";
     return 0;
 }
